40e29d1d9519e6ab1c6e2e22d2312274ef2df45f
Nohemy Ramos <ramosgilma122@gmail.com>
2024-05-31 19:57:27 -0600

    cambio

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
 const NotFound = () => {
+  const navegar ;
   return (
     <div className='mt-5'>NotFound</div>,
-    <div className='mt-5'>NotFound</div>
+    <div className='mt-5'>NotFound</div>,
+    
+    
   )
 }
 
