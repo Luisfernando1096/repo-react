afbabf02ae1acc0bf4c3fc8a5467ff7d3ea8cd4d
Nohemy Ramos <ramosgilma122@gmail.com>
2024-05-31 20:09:55 -0600

    arreglado conflicto

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,20 +1,14 @@
 import React from 'react'
 
 const NotFound = () => {
-  const navegar ;
   return (
-<<<<<<< HEAD
-    <>
+   <>
       <div className='mt-5'>No se encontro la pagina.</div>
       <div className='mt-5'>NotFound</div>,
       <div className='mt-5'>NotFound</div>
+       <div className='mt-5'>NotFound</div>,
+    <div className='mt-5'>NotFound</div>,
     </>
-=======
-    <div className='mt-5'>NotFound</div>,
-    <div className='mt-5'>NotFound</div>,
-    
-    
->>>>>>> ramaNohemy
   )
 }
 
